e75945bcce095bd8bfef70c33cb83469c1b0c726
d-aggarwal <drishtiaggarwal6284@gmail.com>
2026-06-12 16:25:40 +0530

    Add redis and prisms instance

---
+++ user-service/src/utils/prisma.js
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+const connectionString = process.env.DATABASE_URL;
+
+const globalForPrisma = global;
+
+if (!globalForPrisma.prisma) {
+  const adapter = new PrismaPg({ connectionString });
+
+  globalForPrisma.prisma = new PrismaClient({
+    adapter,
+    log: ['error', 'warn'],
+  });
+}
+
+const prisma = globalForPrisma.prisma;
+
+export default prisma;
